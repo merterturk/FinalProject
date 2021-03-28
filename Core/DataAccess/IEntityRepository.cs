47bf7bd0f0deb7d318cdce8545fc963fe684fbf7
Mert <meerterturk@gmail.com>
2021-03-28 21:28:48 +0300

    Add project files.

---
+++ Core/DataAccess/IEntityRepository.cs
@@ -0,0 +1,20 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace Core.DataAccess
+{
+    // T: class , reference tip
+    // IEntity : IEntity olabilir veya implemente eden sınıf olabilir.
+    // new() : new'lenebilir olmalı.
+    public interface IEntityRepository<T> where T:class,IEntity,new()
+    {
+        List<T> GetAll(Expression<Func<T, bool>> filter = null);
+        T Get(Expression<Func<T, bool>> filter);
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+    }
+}
---
+++ DataAccess/Concrete/EntityFramework/NorthwindContext.cs
@@ -0,0 +1,33 @@
+﻿using Core.Entities.Concrete;
+using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+  public class NorthwindContext : DbContext
+    {
+        // Context : Db tabloları ile proje classlarını ilişkilendirmek.
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=LAPTOP-9IQ5NO3T\SQLEXPRESS;Database=NORTHWND;Trusted_Connection=true");
+        }
+
+        public DbSet<Product> Products { get; set; }
+
+        public DbSet<Category> Categories { get; set; }
+
+        public DbSet<Customer> Customers { get; set; }
+
+        public DbSet<Order> Orders { get; set; }
+
+        public DbSet<OperationClaim> OperationClaims { get; set; }
+
+        public DbSet<User> Users { get; set; }
+
+        public DbSet<UserOperationClaim> UserOperationClaims { get; set; }
+
+    }
+}
